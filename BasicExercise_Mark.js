b7530bd75c48d089df86bc1dbf69666c450ac46b
Mark.Etzel@gmx.email <Mark.Etzel@gmx.de>
2020-09-17 10:53:58 +0200

    commenting

--- BasicExercise_Mark.js
+++ BasicExercise_Mark.js
@@ -1,6 +1,10 @@
-var age = 25;
-var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"]
-
+//Exercise 2
 console.log("Hi, my Name is Martin");
+
+//Exercise 3
+var age = 25;
 console.log("And I am " + age + " years old")
+
+//Exercise 4
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"]
 console.log("The most valuable player of the match is " + players[2])
